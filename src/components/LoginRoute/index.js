8b3a7f963389fafb16892757497e6d8d2f686e5a
viday-8 <82586251+viday-8@users.noreply.github.com>
2021-10-07 15:08:04 +0530

    Update index.js

--- src/components/LoginRoute/index.js
+++ src/components/LoginRoute/index.js
@@ -1,5 +1,5 @@
 import {Component} from 'react'
-import {Redirect} from 'react-router-dom'
+import {Redirect, withRouter} from 'react-router-dom'
 import Cookies from 'js-cookie'
 
 import './index.css'
@@ -16,7 +16,6 @@ class Login extends Component {
     const {history} = this.props
     Cookies.set('jwt_token', token, {
       expires: 30,
-      path: '/',
     })
     history.replace('/')
   }
@@ -72,15 +71,16 @@ class Login extends Component {
         </div>
         <div className="loginCon">
           <div className="formCon">
-            <div className="logoCon">
-              <img
-                alt="website logo"
-                src="https://res.cloudinary.com/dclxp4bb4/image/upload/v1632732181/tastyKitchen/Group_7420_vfqhmf.png"
-              />
-              <h1 className="logoName">Tasty Kitchen</h1>
-              <h1 className="loginName">Login</h1>
-            </div>
             <form onSubmit={this.onSubmitForm}>
+              <div className="logoCon">
+                <img
+                  alt="website logo"
+                  src="https://res.cloudinary.com/dclxp4bb4/image/upload/v1632732181/tastyKitchen/Group_7420_vfqhmf.png"
+                />
+                <h1 className="logoName">Tasty Kitchens</h1>
+                <h1 className="loginName">Login</h1>
+              </div>
+
               <div className="inputCon">
                 <label htmlFor="username">USERNAME</label>
                 <input
@@ -120,4 +120,5 @@ class Login extends Component {
   }
 }
 
-export default Login
+export default withRouter(Login)
+
